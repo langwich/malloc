f189cd3f9ab7e068c361ff9764952db6970aaf93
Terence Parr <parrt@cs.usfca.edu>
2015-09-17 13:07:55 -0700

    add test for 2 mallocs

--- freelist/test_basic_sanity.c
+++ freelist/test_basic_sanity.c
@@ -33,17 +33,35 @@ void malloc_2x_word_size() {
 }
 
 void one_malloc() {
-	void *p = malloc(1000); // should split heap into two chunks
+	void *p = malloc(100); // should split heap into two chunks
 	assert_addr_not_equal(p, NULL);
 	Free_Header *freelist = get_freelist();
 	Busy_Header *heap = get_heap_base();
 	assert_addr_not_equal(freelist, heap);
 	// check 1st chunk
 	assert_equal(p, heap);
-	assert_equal(chunksize(p), request2size(1000));
+	assert_equal(chunksize(p), request2size(100));
 	// check 2nd chunk
-	assert_equal(freelist->size, HEAP_SIZE-request2size(1000));
+	assert_equal(freelist->size, HEAP_SIZE-request2size(100));
 	assert_addr_equal(freelist->next, NULL);
+}
+
+void two_malloc() {
+	one_malloc(); // split heap into two chunks and test for sanity.
+	void *p0 = get_heap_base(); // should be first alloc chunk
+	Free_Header *freelist0 = get_freelist();
+	Busy_Header *p = malloc(100); // now split sole free chunk into two chunks
+	assert_addr_not_equal(p, NULL);
+	// check 2nd alloc chunk
+	assert_equal(p, freelist0); // should return previous free chunk
+	assert_equal(chunksize(p), request2size(100));
+	// check remaining free chunk
+	Free_Header *freelist1 = get_freelist();
+	assert_addr_not_equal(freelist0, freelist1);
+	assert_addr_not_equal(freelist0, get_heap_base());
+	assert_equal(chunksize(freelist1), HEAP_SIZE-request2size(100)-request2size(100));
+	assert_equal(chunksize(p0)+chunksize(p)+chunksize(freelist1), HEAP_SIZE);
+	assert_addr_equal(freelist1->next, NULL);
 }
 
 void test_core() {
@@ -78,4 +96,5 @@ int main(int argc, char *argv[]) {
 	test(malloc_word_size);
 	test(malloc_2x_word_size);
 	test(one_malloc);
+	test(two_malloc);
 }
