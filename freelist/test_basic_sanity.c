96a5205502c44b68bfe8a16d8bf63a5d67e1bd31
hanjoes <hanzhou87@gmail.com>
2015-09-17 13:32:53 -0700

    Tweak on project structure.

--- freelist/test_basic_sanity.c
+++ freelist/test_basic_sanity.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "freelist.h"
-#include "../cunit.h"
+#include "cunit.h"
 
 extern void freelist_init(uint32_t max_heap_size);
 extern void freelist_shutdown();
